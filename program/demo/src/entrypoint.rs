421a9eaa4acb3ee1981b8853e8a5e620ba61a75b
ultraspicy <73217673+ultraspicy@users.noreply.github.com>
2022-04-14 20:41:45 -0700

    Add keys

---
+++ program/demo/src/entrypoint.rs
@@ -0,0 +1,17 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg,
+};
+
+entrypoint!(process_instruction);
+fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("process_instruction invoked");
+    Ok(())
+}

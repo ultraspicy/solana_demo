680c5951f5f7317d3bef1a74b891daab2f3d61fb
ultraspicy <73217673+ultraspicy@users.noreply.github.com>
2022-05-28 14:39:43 -0700

    Add comments in lib.rs

--- program/demo/src/lib.rs
+++ program/demo/src/lib.rs
@@ -1,5 +1,11 @@
+/// entrypoint - the entrance of the whole program
 pub mod entrypoint;
+/// error - define the program errors, new error can be added on the fly
 pub mod error;
+/// instructions - define the instructions that we can operate on our state, and their (de)serialization
 pub mod instructions;
+/// processor - the controller, running all business logic
 pub mod processor;
+/// state - define the Account to make our program "stateful"
+/// aka data defines the current state of a running program instance
 pub mod state;

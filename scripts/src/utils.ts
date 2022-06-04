225cafa881807ed5304b2fdcd2c84e9d30a02785
ultraspicy <73217673+ultraspicy@users.noreply.github.com>
2022-06-04 13:40:17 -0700

    Add more utils in utils.ts

--- scripts/src/utils.ts
+++ scripts/src/utils.ts
@@ -1,9 +1,21 @@
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
 
-import * as fs from "fs";
-
+// what does the following 2 line do?
 //@ts-expect-error missing types
 import * as BufferLayout from "buffer-layout";
+
+import * as fs from "fs";
+
+export const logError = (msg: string) => {
+    console.log(`\x1b[31m${msg}\x1b[0m`);
+};
+
+export const writePublicKey = (publicKey: PublicKey, name: string) => {
+    fs.writeFileSync(
+        `./keys/${name}_pub.json`,
+        JSON.stringify(publicKey.toString())
+    );
+};
 
 export const getPublicKey = (name: string) =>
     new PublicKey(
@@ -30,6 +42,22 @@ export const getProgramId = () => {
     }
 }
 
+export const getTerms = (): {
+    aliceExpectedAmount: number;
+    bobExpectedAmount: number;
+} => {
+    return JSON.parse(fs.readFileSync(`./terms.json`) as unknown as string);
+}
+
+export const getTokenBalance = async (
+    pubkey: PublicKey,
+    connection: Connection
+) => {
+    return parseInt(
+        (await connection.getTokenAccountBalance(pubkey)).value.amount
+    );
+};
+
 /**
  * Layout for a public key
  */
@@ -45,17 +73,18 @@ const uint64 = (property = "uint64") => {
     return BufferLayout.blob(8, property);
 };
 
-/**
-    pub struct Demo {
-        pub is_initialized: bool,
-        pub counter: u32,
-    }
-*/
-export const ACCOUNT_DATA_LAYOUT = BufferLayout.struct([
-    BufferLayout.u8("is_initialized"),
-    BufferLayout.u32("counter"),
-//     publicKey("initializerPubkey"),
-//     publicKey("initializerTempTokenAccountPubkey"),
-//     publicKey("initializerReceivingTokenAccountPubkey"),
-//     uint64("expectedAmount"),
-]);
+export const ESCROW_ACCOUNT_DATA_LAYOUT = BufferLayout.struct([
+    BufferLayout.u8("isInitialized"),
+    publicKey("initializerPubkey"),
+    publicKey("initializerTempTokenAccountPubkey"),
+    publicKey("initializerReceivingTokenAccountPubkey"),
+    uint64("expectedAmount"),
+  ]);
+  
+  export interface EscrowLayout {
+    isInitialized: number;
+    initializerPubkey: Uint8Array;
+    initializerReceivingTokenAccountPubkey: Uint8Array;
+    initializerTempTokenAccountPubkey: Uint8Array;
+    expectedAmount: Uint8Array;
+  }
